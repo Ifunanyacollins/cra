363b546e698fc3e2489232af2e006ea34a6ae54b
Collins Ogbuzuru <collinsogbuzuru@gmail.com>
2025-01-11 07:03:42 +0100

    chore: basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-
 
 function App() {
   return (
-    <div className="min-h-screen">
-         Hello world
+    <div className="min-h-screen flex flex-col">
+      <header className="h-14">Header</header>
+      <div className='flex flex-1 bg-black'>
+        <div>sidebar</div>
+        <div>Main content</div>
+        </div>
     </div>
   );
 }
